9f1906aba5947669bfe2f226b22489ef2223c33e
sojibpml <it.powermanlocal02@gmail.com>
2026-09-16 10:59:25 +0600

    Add unit test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Tests\Unit;
+use PHPUnit\Framework\TestCase;
+class ExampleTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
